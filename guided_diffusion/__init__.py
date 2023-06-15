d7ca9051ae1c0db238b75dd6ecd4307ac3ddb3d5
Bhavay192 <56443877+Bhavay192@users.noreply.github.com>
2023-06-15 10:57:17 +0530

    Update __init__.py

--- guided_diffusion/__init__.py
+++ guided_diffusion/__init__.py
@@ -45,7 +45,7 @@ def create_diffusion(
             if not learn_sigma
             else gd.ModelVarType.LEARNED_RANGE
         ),
-        loss_type=loss_type
+        loss_type=loss_type,
         rescale_timesteps=rescale_timesteps
     )
 
